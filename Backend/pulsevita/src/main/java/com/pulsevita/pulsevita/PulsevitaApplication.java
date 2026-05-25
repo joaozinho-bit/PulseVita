3223b4aa717756a693a4e828373685879343f6ca
Rileyzik <rileyeduardo451@gmail.com>
2026-05-25 12:07:25 +0100

    BackEnd adicionado

---
+++ Backend/pulsevita/src/main/java/com/pulsevita/pulsevita/PulsevitaApplication.java
@@ -0,0 +1,13 @@
+package com.pulsevita.pulsevita;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulsevitaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulsevitaApplication.class, args);
+	}
+
+}
---
+++ Backend/pulsevita/src/test/java/com/pulsevita/pulsevita/PulsevitaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulsevita.pulsevita;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulsevitaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
